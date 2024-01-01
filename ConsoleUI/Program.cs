9a1d0434c32c9e3851262ad8e986ac104f8ef3be
ömür furkan kurt <omurfurkankurt@gmail.com>
2024-01-01 20:28:41 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Net.Http.Headers;
+
+namespace ConsoleUI
+{    
+    // SOLID
+    // Open Closed Principle 
+    // Yazılıma Yeni bir özelik eklenirse mevcutaki hiçbir koda dokunulmaz
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var Product in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(Product.ProductName);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint generic kısıtlar
+    // T : class = T bir referans tip olmalı anlamına gelir ,
+    // aynı zamanda  IEntity türünde olabilr veya IEntity implemente eden bir nesne olabilir,
+    // new' lenebilr olmalı Yani interface olamaz 
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
