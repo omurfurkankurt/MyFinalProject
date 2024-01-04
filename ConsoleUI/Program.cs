20a4fd21f54079b7a89c485377da4cb75e34156d
ömür furkan kurt <omurfurkankurt@gmail.com>
2024-01-04 20:02:26 +0300

    Core

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,14 +13,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+           // ProductTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var Product in productManager.GetByUnitPrice(50,100))
+            foreach (var Product in productManager.GetByUnitPrice(50, 100))
             {
                 Console.WriteLine(Product.ProductName);
 
             }
-
-            Console.ReadLine();
         }
     }
 }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,22 +1,23 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities;
 
-namespace DataAccess.Abstract
+// Core katmanı diger katmanları referans almaz
+
+namespace Core.DataAccess
 {
     // generic constraint generic kısıtlar
     // T : class = T bir referans tip olmalı anlamına gelir ,
     // aynı zamanda  IEntity türünde olabilr veya IEntity implemente eden bir nesne olabilir,
     // new' lenebilr olmalı Yani interface olamaz 
-    public interface IEntityRepository<T> where T : class,IEntity,new()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll(Expression<Func<T,bool>> filter=null);
-        T Get(Expression<Func<T,bool>> filter);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
